30afe97b4fc14697c549340cd8698078f12d65e7
alejosangel <104403106+alejosangel@users.noreply.github.com>
2022-05-18 06:30:23 -0500

    Update CrudTest.java

--- src/test/java/proyecto/CrudTest.java
+++ src/test/java/proyecto/CrudTest.java
@@ -85,7 +85,7 @@ public class CrudTest {
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Hola Hola 2 Modificacion'])[2]/following::button[1]")).click();
     //Assert - eliminar info
     //assertThat(driver.findElement(By.xpath("html/body/div/div/div[2]/table/tbody/tr[1]/td[1]")).getText(),is("Hola Hola 2 Modificacion"));
-    assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Hola Hola 2 Modificacion[\\s\\S]*$"));
+    assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*hola hola 2[\\s\\S]*$"));
   }
 
   @After
